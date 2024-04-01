ea925a6780adb6dba1376a19a5b45eb932cac880
Thalyson <128197405+ThalysonE@users.noreply.github.com>
2024-04-01 19:49:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct data{
-    int dia;
-    int mes;
-    int ano;
-};
-
 typedef struct voo{
     int numVoo;
-    char origem[20];
-    char destino[20];
-    struct data dtVoo; // pode utilizar uma biblioteca
+    char origem[30];
+    char destino[30];
+    char data[10];
     int horario; // numero de quatro digitos
-    int numAssentosDisp;
+    int numAssentos;
+    struct voo *esquerda,*direita
+    short altura;
 } Voo;
-
-Voo inserirDados(){
-    Voo novo;
-    printf("\nDigite o número do voo: ");
-    scanf("%d", &novo.numVoo);
-    printf("\nDigite o nome da origem do voo: ");
-    scanf(" %19[^\n]", novo.origem); 
-    printf("\nDigite o destino do Voo: ");
-    scanf(" %19[^\n]", novo.destino); 
-    printf("\nDigite o horario do voo: ");
-    scanf("%d", &novo.horario);
-    printf("Digite o dia do Voo no formato (dd/mm/aa)");
-    scanf("%d/%d/%d", &novo.dtVoo.dia, &novo.dtVoo.mes, &novo.dtVoo.ano);
-    printf("\nDigite o número de assentos disponiveis no voo: ");
-    scanf("%d", &novo.numAssentosDisp);
-    return novo;
-}
